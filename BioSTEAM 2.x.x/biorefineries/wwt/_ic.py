9ff54a7637ba89aae58a77e609585800fb866217
Yalin Li <zoe.yalin.li@gmail.com>
2021-07-02 14:54:16 -0500

    file organization

---
+++ BioSTEAM 2.x.x/biorefineries/wwt/_ic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 11:25:13 2021
+
+@author: Yalin Li
+"""
+
+'''
+Current process algorithms based on the M.S. thesis of George Kontos:
+
+'''
